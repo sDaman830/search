c827254aa016c104b4602101de1355f94b489917
sDaman830 <99036076+sDaman830@users.noreply.github.com>
2024-06-11 17:03:34 +0530

    final

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.use(bodyParser.json());
 app.use(cors());
